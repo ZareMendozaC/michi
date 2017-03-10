3e304ca7976917339c15ac65fbf9d9d50e8b7e92
ZareMendozaC <zare.mendozac@gmail.com>
2017-03-10 17:01:44 -0500

    dibujando tablero

--- js/tablero.js
+++ js/tablero.js
@@ -1,3 +1,4 @@
+$('#0').click(DrawMovimiento);
 $('#1').click(DrawMovimiento);
 $('#2').click(DrawMovimiento);
 $('#3').click(DrawMovimiento);
@@ -6,8 +7,53 @@ $('#5').click(DrawMovimiento);
 $('#6').click(DrawMovimiento);
 $('#7').click(DrawMovimiento);
 $('#8').click(DrawMovimiento);
-$('#9').click(DrawMovimiento);
+
+var celdas=[0,0,0,0,0,0,0,0,0];
+var win1= [1,1,1,0,0,0,0,0,0];
+var win2= [0,0,0,1,1,1,0,0,0];
+var win3= [0,0,0,0,0,0,1,1,1];
+var numJugadas= 9;
+var jugador1=[];
+var jugador2=[];
+var turnA = true;
 function DrawMovimiento(){
-	//alert(this.id);
-	$(this).addClass("animated flipOutY");
+	posicion=this.id;
+	marcar(posicion);
+	$(this).find('#cubo').addClass("animated flipOutY");
+}
+function estaMarcado(posicion)
+{
+	if(celdas[posicion]!=0)
+		return true;
+	else
+		return false;
+}
+function marcar(posicion)
+{
+	if(turnA)
+	{
+		if(estaMarcado(posicion)==false)
+		{
+			celdas[posicion]="a";
+			$('#'+posicion).find('img').remove();
+			$('#'+posicion).append('<img class= "animated bounce" src="img/star.png"/>');
+		}
+		else
+		{
+			alert("esa posición ya esta marcada!");
+		}
+	}else{
+		if(estaMarcado(posicion)==false)
+		{
+		celdas[posicion]="b";
+		$('#'+posicion).find('img').remove();
+		$('#'+posicion).append('<img class= "animated bounce" src="img/flor.png"/>');
+		}
+		else
+		{
+			alert("esa posición ya esta marcada!");
+		}
+	}
+	console.log(celdas)
+	turnA = !turnA;	
 }
